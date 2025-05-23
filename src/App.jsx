929321fc4f36df2757db9fa1db7e7439cd7c6c0a
Abhay Dwivedi <abhaydwivedi4504@gmail.com>
2025-05-24 00:53:35 +0530

    Added MealsApi component and updated App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,16 @@
 import React from "react";
 // import Counter from "./Components/Counter";
 import Todo from "./Components/Todo";
+import MealsApi from "./Components/MealsApi";
+// import MealsApi from "./Compponents/MealsApi";
 
 const App = () => {
   return (
     <div>
       {/* <Counter /> */}
       <Todo />
+      {/* <MealsApi /> */}
+      <MealsApi />
     </div>
   );
 };
