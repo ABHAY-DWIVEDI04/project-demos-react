a5c73cf2172301d5426af5c829129fc65fea10f2
Abhay Dwivedi <abhaydwivedi4504@gmail.com>
2025-05-24 13:10:56 +0530

    Added CalcuLatore component and updated App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,18 @@
 import React from "react";
+import Calculator from "./Components/Calculator";
 // import Counter from "./Components/Counter";
-import Todo from "./Components/Todo";
-import MealsApi from "./Components/MealsApi";
+// import Todo from "./Components/Todo";
+// import MealsApi from "./Components/MealsApi";
 // import MealsApi from "./Compponents/MealsApi";
 
 const App = () => {
   return (
     <div>
       {/* <Counter /> */}
-      <Todo />
+      {/* <Todo /> */}
       {/* <MealsApi /> */}
-      <MealsApi />
+      {/* <MealsApi /> */}
+      <Calculator />
     </div>
   );
 };
